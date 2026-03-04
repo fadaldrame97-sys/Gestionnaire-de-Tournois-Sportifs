91f555f0584b8533e688fd8cec385024376d6350
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-03-04 10:45:46 +0000

    Importer le Header dans le App

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header(){
+    return(
+        <header>
+    <div>
+         <h1>Good Morning</h1>
+         <h1>Fadal Dramé</h1> 
+
+    </div>
+        </header>
+    )
+}
