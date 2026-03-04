d4df29d22b8faa3384ee324c9c4d43ddb22a09c4
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-03-04 11:27:02 +0000

    Configurer le Tailwind pour comeencer le style

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,12 +1,14 @@
 import Notif from "./assets/Notif.png";
+import Fadal from "./assets/Fadal.jpg"
 export default function Header(){
     return(
-        <header>
+        <header style={{ backgroundColor: "black", color: "white" }}>
     <div>
          <h1>Good Morning</h1>
          <h1>Fadal Dramé</h1> 
     
-          <img src={Notif} alt="Notification icon" width="24" />
+        <img src={Notif} alt="Notification icon" width="24" />
+        <img src={Fadal} alt="Notification icon" width="24" />
 
     </div>
 
