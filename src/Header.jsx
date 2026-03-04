3bbe8c432bc3fe813b1f763286a27657291e339b
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-03-04 11:05:27 +0000

    Ajouter l'icon de notification

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,15 @@
+import Notif from "./assets/Notif.png";
 export default function Header(){
     return(
         <header>
     <div>
          <h1>Good Morning</h1>
          <h1>Fadal Dramé</h1> 
+    
+          <img src={Notif} alt="Notification icon" width="24" />
 
     </div>
+
         </header>
     )
 }
