a5c28007497dddf9be26f20a3829007b19a73069
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-03-06 11:49:40 +0000

    Importer les link du Rooter dans Home et Détails

--- src/Components/Détails.jsx
+++ src/Components/Détails.jsx
@@ -1,4 +1,7 @@
 import Header from "./Header";
+import { tournamentData } from "./data/tournament";
+import  PremierCarte from "./Components/TournementONE";
+import { Link } from "react-router-dom";
 export default function Details(){
 return (
 <div></div>
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,8 +1,12 @@
 import Header from "./Header";
+import { tournamentData } from "./data/tournament";
+import  PremierCarte from "./Components/TournementONE";
+import { Link } from "react-router-dom";
 export default function Home(){
     return(
         <div>
-            
+         <Header/>
+
         </div>
     );
 }
