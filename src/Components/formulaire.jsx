3fc4c7fa17a4e22f4e411358791d551e732ff897
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-03-13 14:32:06 +0000

    Initialiser le useState pour les élément du formulaire

---
+++ src/Components/formulaire.jsx
@@ -0,0 +1,8 @@
+import { useState } from "react";
+export default function formulaire(){
+
+    const [Nom, setNom]=useState("")
+    const [Equipe,setEquipe]=useState("")
+    const [Niveau,setNiveau]=useState("")
+    return();
+}
