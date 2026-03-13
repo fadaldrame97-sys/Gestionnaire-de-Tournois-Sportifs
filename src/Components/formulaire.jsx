cd5348140e47735589022d452cbcc66c879522a7
fadaldrame97-sys <fadaldrame97@gmail.com>
2026-03-13 15:11:47 +0000

    Une fonction pour créer un object et une pour Réinitialiser le formulaire

--- src/Components/formulaire.jsx
+++ src/Components/formulaire.jsx
@@ -1,8 +1,29 @@
 import { useState } from "react";
-export default function formulaire(){
+export default function Formulaire(){
 
     const [Nom, setNom]=useState("")
     const [Equipe,setEquipe]=useState("")
     const [Niveau,setNiveau]=useState("")
+
+
+    function Soumettre(e){
+        e.preventDefault();
+
+        const nouveauParticipant= {
+            id: Date.now(),
+            name:Nom,
+            Equipe:Equipe,
+            Niveau:Niveau
+        };
+    }
+
+    function AjouterUnParticipant(){
+
+   setNom("");
+   setEquipe("");
+   setNiveau("");
+
+
+    }
     return();
 }
